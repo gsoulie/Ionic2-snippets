98b5bb7bbb61d2df55584dc7c9ade52c7001d201
gsoulie <gsoulie@users.noreply.github.com>
2020-10-21 10:42:32 +0200

    Update tool-service.ts

--- tool-service.ts
+++ tool-service.ts
@@ -9,6 +9,17 @@ export class ToolProvider {
   constructor(private file: File, 
               public alertCtrl: AlertController,
               public toastCtrl: ToastController) {}
+  
+  /**
+   * Generate unique GUID string
+   */
+  generateGUID() {
+    const guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r&0x3 | 0x8);
+      return v.toString(16);
+    });
+    return guid;
+  }
 
   /**
    * Searching specific member in array
