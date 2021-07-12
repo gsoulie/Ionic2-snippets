c3063c020551f8bfb7be758e6dfe924a68428908
gsoulie <gsoulie@users.noreply.github.com>
2021-07-12 14:57:05 +0200

    Update tool-service.ts

--- tool-service.ts
+++ tool-service.ts
@@ -10,6 +10,27 @@ export class ToolProvider {
               public alertCtrl: AlertController,
               public toastCtrl: ToastController) {}
   
+  
+  private hashCode(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  }
+
+  /**
+   *
+   * usage : this.intToRGB(this.hashCode(username))
+   */
+  private intToRGB(i) {
+    const c = (i & 0x00FFFFFF)
+    .toString(16)
+    .toUpperCase();
+
+    return '#' + '00000'.substring(0, 6 - c.length) + c;
+  }
+  
   /**
    * Generate unique GUID string
    */
